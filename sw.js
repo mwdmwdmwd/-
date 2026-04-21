6d2c71eab5167c7f16a9f712310e3f04d5b1221c
mwdmwdmwd <lovezelda2@gmail.com>
2026-04-21 05:50:37 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,25 @@
-const CACHE_NAME = 'brick-breaker-love-v2';
+const CACHE_NAME = 'yumi-brick-breaker-v3';
 const ASSETS = [
   './',
   './index.html',
+  './styles.css',
+  './game.js',
   './manifest.json',
-  './sw.js',
   './icon-192.png',
   './icon-512.png',
   './apple-touch-icon.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
